fd75f81ce99136b98c2a577d34eb45c1e0ea9410
Tushar Islam <tushar060703@gmail.com>
2026-05-08 11:09:13 -0400

    Phase 4 completed

--- src/main/java/com/pluralsight/ui/Program.java
+++ src/main/java/com/pluralsight/ui/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight.ui;
 
 public class Program {
     static void main() {
-        
+        UserInterface userInterface = new UserInterface();
+        userInterface.display();
     }
 }
