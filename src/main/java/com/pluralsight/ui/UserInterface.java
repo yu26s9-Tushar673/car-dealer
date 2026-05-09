3ce41a5714697ef9091576be16d12e6e2657d665
Tushar Islam <tushar060703@gmail.com>
2026-05-09 18:12:44 -0400

    Phase 5 completed

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -56,7 +56,7 @@ public class UserInterface {
         if (vehicles.isEmpty()){
             System.out.println("No vehicles found.");
         }
-        System.out.println("-------- Inventory --------");
+        System.out.println("-------- Matching Inventory --------");
         for (Vehicle v : vehicles) {
             System.out.printf("VIN: %d | %d %s %s | Type: %s | Color: %s | Miles: %d | Price: $%.2f%n",
                     v.getVin(), v.getYear(), v.getMake(), v.getModel(),
@@ -65,21 +65,37 @@ public class UserInterface {
     }
 
     private void processGetByPriceRequest() {
+        double min = Console.promptForDouble("Enter Minimum Price: ");
+        double max = Console.promptForDouble("Enter Maximum Price: ");
+        displayVehicles(dealership.getVehiclesByPrice(min, max));
     }
 
     private void processGetByMakeModelRequest() {
+        String make = Console.promptForString("Enter Vehicle Make: ");
+        String model = Console.promptForString("Enter Vehicle Model: ");
+        displayVehicles(dealership.getVehiclesByMakeModel(make, model));
     }
 
     private void processGetByYearRequest() {
+        int min = Console.promptForInt("Enter Minimum Year: ");
+        int max = Console.promptForInt("Enter Maximum Year: ");
+        displayVehicles(dealership.getVehiclesByYear(min, max));
     }
 
     private void processGetByColorRequest() {
+        String color = Console.promptForString("Enter Color: ");
+        displayVehicles(dealership.getVehiclesByColor(color));
     }
 
     private void processGetByMileageRequest() {
+        int min = Console.promptForInt("Enter Minimum Mileage: ");
+        int max = Console.promptForInt("Enter Maximum Mileage: ");
+        displayVehicles(dealership.getVehiclesByMileage(min, max));
     }
 
     private void processGetByVehicleTypeRequest() {
+        String type = Console.promptForString("Enter Type Of Vehicle (car, truck, suv, etc): ");
+        displayVehicles(dealership.getVehiclesByType(type));
     }
 
     private void processGetAllVehiclesRequest() {
@@ -88,9 +104,36 @@ public class UserInterface {
     }
 
     private void processAddVehicleRequest() {
+        int vin = Console.promptForInt("Enter Vehicle VIN#: ");
+        int year = Console.promptForInt("Enter Vehicle Year: ");
+        String make = Console.promptForString("Enter Vehicle Make: ");
+        String model = Console.promptForString("Enter Vehicle Model: ");
+        String type = Console.promptForString("Enter Vehicle Type (Car, Suv, Truck.): ").toUpperCase();
+        String color = Console.promptForString("Enter Vehicle Color: ");
+        int odometer = Console.promptForInt("Enter Vehicle Mileage: ");
+        double price = Console.promptForDouble("Enter Vehicle Price: ");
+
+        dealership.addVehicle(new Vehicle(vin, year, make, model, type, color, odometer, price));
+        new DealershipFileManager("inventory.csv").saveDealership(dealership);
+        System.out.println("Vehicle Added to Inventory.");
     }
 
     private void processRemoveVehicleRequest() {
+        int vin = Console.promptForInt("Enter Vehicle VIN # to remove: ");
+        for (Vehicle v : dealership.getAllVehicles()) {
+            if (v.getVin() == vin) {
+                boolean confirm = Console.promptForYesNo("Are you sure you would like to remove Vehicle with VIN# " + v.getVin() + ": ");
+                if (confirm) {
+                    dealership.removeVehicle(v);
+                    new DealershipFileManager("inventory.csv").saveDealership(dealership);
+                    System.out.println("Vehicle Removed from Inventory.");
+                } else {
+                    System.out.println("Removal Cancelled.");
+                }
+                return;
+            }
+        }
+        System.out.println("Vehicle with VIN #" + vin + " not found.");
     }
 
 }
